ffa5fb4b59ae224c4f364d767d53a69183ec72b7
Remison Junio <remisonpires@gmail.com>
2022-01-28 18:48:25 -0300

    Seção Footer, Seção contact, js da seção services e dos titulos principais de cada seção

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+//Menu Botão.
+
 const btnHamburger = document.querySelector('.btn-hamburger');
 btnHamburger.addEventListener('click', ()=> {
 	const menu = document.querySelector('.menu');
 	menu.classList.toggle('active');
 })
+
+//Títulos
+
+const efeitoTitulo = document.querySelectorAll('.efeitoTitulo');
+efeitoTitulo.forEach(function(elemento){
+	efeitoMouse(elemento);
+})
+
+//Cards
+
+const card = document.querySelectorAll('.card');
+card.forEach(function(elemento) {
+	efeitoMouse(elemento);
+})
+
+
+//Função que cria o efeito ao passar o mouse por cima do elemento.
+
+function efeitoMouse(elemento) {
+	elemento.addEventListener('mouseenter', ()=> {
+		elemento.classList.add('active');
+	})
+
+	elemento.addEventListener('mouseleave', ()=> {
+		elemento.classList.remove('active');
+	})
+}
