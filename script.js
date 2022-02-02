6c1fca3d6b4f0af81db616c011bbdc2f59f04c7c
Remison Junio <remisonpires@gmail.com>
2022-02-01 22:24:00 -0300

    JavaScript da seção home, e início da resposividade com Media Queries

--- script.js
+++ script.js
@@ -6,24 +6,55 @@ btnHamburger.addEventListener('click', ()=> {
 	menu.classList.toggle('active');
 })
 
-//Títulos
+//Animação da home
 
-const efeitoTitulo = document.querySelectorAll('.efeitoTitulo');
-efeitoTitulo.forEach(function(elemento){
-	efeitoHover(elemento);
-})
+function animacaoHome() {
+	const homeAdItem = document.querySelectorAll('.home-ad-item');
+	
+	function removendoActive(elemento) {
+		elemento.classList.remove('active');
+	}
 
-//Cards
+	function adicionandoActive(elemento){
+		elemento.classList.add('active');
+	}
 
-const card = document.querySelectorAll('.card');
-card.forEach(function(elemento) {
-	efeitoHover(elemento);
-})
+	let c = 0
+
+	for(let i = 0; i < 100000 ; i++){
+		(function (){
+			setTimeout(function(){
+
+				if(c == 0){
+					removendoActive(homeAdItem[2])
+					adicionandoActive(homeAdItem[0])
+					c++
+					
+					
+				} else if(c == 1) {
+					removendoActive(homeAdItem[0])
+					adicionandoActive(homeAdItem[1])
+					c++
+					
+				} else if(c == 2) {
+					removendoActive(homeAdItem[1])
+					adicionandoActive(homeAdItem[2])
+
+					c = 0	
+				}
+
+				console.log(c)
+			}, 5000 * i);
+		})()
+	}
+}
+
+animacaoHome();
 
 //Botões da seção portfolio
 
 const btnPortfolio = document.querySelectorAll('.portfolio-btn');
-var allItem = document.querySelectorAll('.all-item');
+const allItem = document.querySelectorAll('.all-item');
 const brandItem = document.querySelectorAll('.brand-item');
 const designItem = document.querySelectorAll('.design-item');
 const graphicItem = document.querySelectorAll('.graphic-item');
@@ -60,37 +91,6 @@ btnPortfolio.forEach(function(elemento){
 	})
 })
 
-//Portfolio album textos
-
-const albumItem = document.querySelectorAll('.album-item');
-const albumTituloImg = document.querySelectorAll('.titulo-img');
-
-albumItem.forEach(function(elemento){
-	efeitoHover(elemento);
-})
-
-
-//Blog 
-
-const blogItem = document.querySelectorAll('.blog-conteudo-item');
-blogItem.forEach(function(elemento){
-	efeitoHover(elemento);
-})
-
-
-
-//Função que cria o efeito ao passar o mouse por cima do elemento.
-
-function efeitoHover(elemento) {
-	elemento.addEventListener('mouseenter', ()=> {
-		elemento.classList.add('active');
-	})
-
-	elemento.addEventListener('mouseleave', ()=> {
-		elemento.classList.remove('active');
-	})
-}
-
 //Função que mostra as imagem do álbum
 
 function mostrarAlbum(item) {
@@ -100,6 +100,7 @@ function mostrarAlbum(item) {
 }
 
 //Função que ocuta as imagem do álbum
+
 function ocutarAlbum(item) {
 	item.forEach(function(elemento){
 		elemento.classList.remove('mostra');
